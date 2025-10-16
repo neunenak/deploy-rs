1a4a690ad058c7e656b22e0f33e696735bf2ad0f
NAHO <90870942+trueNAHO@users.noreply.github.com>
2025-10-16 19:08:00 +0200

    Remove redundant aarch64-darwin system already present by default
    Remove the explicitly added aarch64-darwin system that is already
    included in the default systems input.
    
    Maybe this was not the case at the time of commit ed1ee1d86684 ("add
    aarch64-darwin to built systems") from four years ago.
    
    Reverts: ed1ee1d86684 ("add aarch64-darwin to built systems")

--- flake.nix
+++ flake.nix
@@ -155,7 +155,7 @@
       };
     };
   } //
-    flake-utils.lib.eachSystem (flake-utils.lib.defaultSystems ++ ["aarch64-darwin"]) (system:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
