67a720a86ddf51854827491318173197c61b3fbb
Greg Shuflin <greg@everydayimshuflin.com>
2026-02-04 23:21:51 -0800

    update merge crate

--- src/data.rs
+++ src/data.rs
@@ -10,8 +10,12 @@ use std::path::PathBuf;
 #[derive(Deserialize, Debug, Clone, Merge)]
 pub struct GenericSettings {
     #[serde(rename(deserialize = "sshUser"))]
+    #[merge(strategy = merge::option::overwrite_none)]
     pub ssh_user: Option<String>,
+
+    #[merge(strategy = merge::option::overwrite_none)]
     pub user: Option<String>,
+
     #[serde(
         skip_serializing_if = "Vec::is_empty",
         default,
@@ -19,25 +23,45 @@ pub struct GenericSettings {
     )]
     #[merge(strategy = merge::vec::append)]
     pub ssh_opts: Vec<String>,
+
     #[serde(rename(deserialize = "compress"))]
+    #[merge(strategy = merge::option::overwrite_none)]
     pub compress: Option<bool>,
+
     #[serde(rename(deserialize = "fastConnection"))]
+    #[merge(strategy = merge::option::overwrite_none)]
     pub fast_connection: Option<bool>,
+
     #[serde(rename(deserialize = "autoRollback"))]
+    #[merge(strategy = merge::option::overwrite_none)]
     pub auto_rollback: Option<bool>,
+
     #[serde(rename(deserialize = "confirmTimeout"))]
+    #[merge(strategy = merge::option::overwrite_none)]
     pub confirm_timeout: Option<u16>,
+
     #[serde(rename(deserialize = "activationTimeout"))]
+    #[merge(strategy = merge::option::overwrite_none)]
     pub activation_timeout: Option<u16>,
+
     #[serde(rename(deserialize = "tempPath"))]
+    #[merge(strategy = merge::option::overwrite_none)]
     pub temp_path: Option<PathBuf>,
+
     #[serde(rename(deserialize = "magicRollback"))]
+    #[merge(strategy = merge::option::overwrite_none)]
     pub magic_rollback: Option<bool>,
+
     #[serde(rename(deserialize = "sudo"))]
+    #[merge(strategy = merge::option::overwrite_none)]
     pub sudo: Option<String>,
+
     #[serde(default,rename(deserialize = "remoteBuild"))]
+    #[merge(strategy = merge::option::overwrite_none)]
     pub remote_build: Option<bool>,
+
     #[serde(rename(deserialize = "interactiveSudo"))]
+    #[merge(strategy = merge::option::overwrite_none)]
     pub interactive_sudo: Option<bool>,
 }
 
